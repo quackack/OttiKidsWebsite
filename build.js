66ff259326517a0320c3d17d75c7dad8889093c7
joshua cook <jac22855@utexas.edu>
2023-09-23 21:43:36 -0500

    Have a basic version of the whole website, with a wiki section, and a book section, and a common header that gets built into every html page.

--- build.js
+++ build.js
@@ -4,22 +4,25 @@ const OUT_DIR = "./build";
 const SRC_DIR = "./src/root";
 const HEADER_DIR = "./src/templates/header.html";
 const HEADER_LOCATION_STRING = "<!--{header.html}-->";
+const DEPTH_PLACEHOLDER = "<!--depth-->";
 
-function setUpBuild() {
+function cleanUpBuild() {
 	if (fs.existsSync(OUT_DIR)) {
 		fs.rmSync(OUT_DIR, { recursive: true, force: true });
 	}
-	fs.mkdirSync(OUT_DIR);
 }
-setUpBuild();
+cleanUpBuild();
 
-const headerHTML = fs.readFileSync(HEADER_DIR);
+const headerHTML = fs.readFileSync(HEADER_DIR).toString('utf8');
 
-function copyWithProcessing(outRoot, sourceRoot, currentFile) {
+function copyWithProcessing(outRoot, sourceRoot, currentFile, depth) {
 	var fullSourcePath = sourceRoot + "/" + currentFile;
+	var fullOutPath = outRoot + "/" + currentFile;
 	if (fs.lstatSync(fullSourcePath).isDirectory()) {
+		fs.mkdirSync(fullOutPath);
+		
 		for (file of fs.readdirSync(fullSourcePath)) {
-			copyWithProcessing(outRoot, sourceRoot, currentFile + "/" + file);
+			copyWithProcessing(outRoot, sourceRoot, currentFile + "/" + file, depth + 1);
 		}
 		return;
 	}
@@ -30,9 +33,19 @@ function copyWithProcessing(outRoot, sourceRoot, currentFile) {
 			return;
 		}
 		//Put in the header.
-		let fixedData = data.replace(HEADER_LOCATION_STRING, headerHTML);
+		let fixedHeader = formatHeaderForDepth(depth);
+		let fixedData = data.replace(HEADER_LOCATION_STRING, fixedHeader);
 		
-		fs.writeFileSync(outRoot + "/" + currentFile, fixedData);
+		fs.writeFileSync(fullOutPath, fixedData);
 	});
 }
-copyWithProcessing(OUT_DIR, SRC_DIR, "");
+
+function formatHeaderForDepth(depth) {
+	var depthString = ".";
+	for (var i = 0; i < depth; i++) {
+		depthString += "/..";
+	}
+	return headerHTML.replaceAll(DEPTH_PLACEHOLDER, depthString);
+}
+
+copyWithProcessing(OUT_DIR, SRC_DIR, "", -1);
